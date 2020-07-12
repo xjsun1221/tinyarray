97af09063bfeabe13589398e0af5da49d3650a2e
Bioinfoplanet <Bioplanet520@outlook.com>
2020-07-12 22:48:45 +0800

    first upload

---
+++ R/t_choose.R
@@ -0,0 +1,55 @@
+##' t_choose
+##'
+##' choose diffrencial expressed genes by simple t.test
+##'
+##' @inheritParams get_deg
+##' @param genes a vector with some genes
+##' @param up_only keep up genes in the result only
+##' @param down_only keep down genes in the result only
+##' @return a vector with diffrencial expressed genes
+##' @author Xiaojie Sun
+##' @examples
+##' exp = matrix(rnorm(1000),ncol = 10)
+##' rownames(exp) = sample(mRNA_annov23$gene_id,100)
+##' colnames(exp) = c(paste0("TCGA",1:5),paste0("GTEX",1:5))
+##' k  = trans_exp(exp)
+##' k[,1:5] = k[,1:5]+10
+##' exp = k
+##' group_list = rep(c("A","B"),each = 5)
+##' genes = sample(rownames(k),3)
+##' t_choose(genes,k,group_list)
+##' @seealso
+##' \code{\link{geo_download}};\code{\link{draw_volcano}};\code{\link{draw_venn}}
+
+
+t_choose <- function(genes,exp,group_list,up_only = F,down_only = F){
+  table(genes %in% rownames(exp))
+  exp_small = exp[rownames(exp) %in% genes,]
+  dat = data.frame(t(exp_small))
+  dat$group_list = group_list
+  p_v <- sapply(1:(ncol(dat)-1), function(i){
+    t.test(dat[,i] ~group_list)$p.value
+  })
+  names(p_v) = colnames(dat)[-ncol(dat)]
+  table(p_v<0.05)
+  exp_genes = names(p_v[p_v < 0.05])
+  if(up_only){
+    es_up <- sapply(1:(ncol(dat)-1), function(i){
+      tmp = t.test(dat[,i] ~group_list)
+      k = tmp$estimate[2]-tmp$estimate[1] >0
+      return(k)
+    })
+    up_genes = genes[p_v <0.05 & es_up]
+    return(up_genes)
+  }else if(down_only){
+    es_down <- sapply(1:(ncol(dat)-1), function(i){
+      tmp = t.test(dat[,i] ~group_list)
+      k = tmp$estimate[2]-tmp$estimate[1] <0
+      return(k)
+    })
+    down_genes = genes[p_v <0.05 & es_down]
+    return(down_genes)
+  }else{
+    return(exp_genes)
+  }
+}
---
+++ R/trans_exp.R
@@ -0,0 +1,60 @@
+##' trans_exp
+##'
+##' transform rownames of tcga or tcga_gtex expression set from gdc or xena,from ensamble id to gene symbol
+##'
+##' @param exp tcga or tcga_gtex expression set from gdc or xena
+##' @param mrna_only only keep mrna rows in result
+##' @param lncrna_only only keep lncrna rows in result
+##' @return a transformed expression set with symbol
+##' @author Xiaojie Sun
+##' @importFrom stringr str_detect
+##' @importFrom dplyr inner_join
+##' @export
+##' @examples
+##' exp = matrix(rnorm(1000),ncol = 10)
+##' rownames(exp) = sample(mRNA_annov23$gene_id,100)
+##' colnames(exp) = c(paste0("TCGA",1:5),paste0("GTEX",1:5))
+##' k  = trans_exp(exp)
+
+##' @seealso
+##' \code{\link{simpd}};\code{\link{draw_volcano}};\code{\link{draw_venn}}
+
+trans_exp = function(exp,mrna_only = F,lncrna_only = F){
+  k00 = any(str_detect(colnames(exp),"TCGA"))
+  if(!k00)warning("this expression set probably not from TCGA,please ensure it")
+  k0 = any(str_detect(colnames(exp),"GTEX"))
+  if(!k0){
+    lanno = lnc_anno
+    manno = mRNA_anno
+  }else{
+    lanno = lnc_annov23
+    manno = mRNA_annov23
+    }
+  n1 = sum(rownames(exp) %in% manno$gene_id)
+  k1 = length(n1)/nrow(exp)< 0.25 & length(n1)<5000
+  n2 = sum(rownames(exp) %in% lanno$gene_id)
+  k2 = length(n2)/nrow(exp)< 0.25 & length(n2)<5000
+  message(paste0(n1,
+                 " of genes successfully mapping to mRNA,",
+                 n2,
+                 " of genes successfully mapping to lncRNA,"))
+  mRNA_exp = exp[rownames(exp) %in% manno$gene_id,]
+  tmp = data.frame(gene_id = rownames(exp))
+  x = dplyr::inner_join(tmp,manno,by = "gene_id")
+  mRNA_exp = mRNA_exp[!duplicated(x$gene_name),]
+  x = x[!duplicated(x$gene_name),]
+  rownames(mRNA_exp) = x$gene_name
+  lnc_exp = exp[rownames(exp) %in% lanno$gene_id,]
+  tmp = data.frame(gene_id = rownames(exp))
+  x = dplyr::inner_join(tmp,lanno,by = "gene_id")
+  lnc_exp = lnc_exp[!duplicated(x$gene_name),]
+  x = x[!duplicated(x$gene_name),]
+  rownames(lnc_exp) = x$gene_name
+  if(mrna_only){
+    return(mRNA_exp)
+  }else if(lncrna_only){
+      return(lnc_exp)
+  }else{
+      return(rbind(mRNA_exp,lnc_exp))
+    }
+  }
