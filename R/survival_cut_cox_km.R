792edec99d6908a2e0d0b75749c0799f4c7076f5
Bioinfoplanet <Bioplanet520@outlook.com>
2020-07-13 13:41:02 +0800

    first upload

---
+++ R/survival_cut_cox_km.R
@@ -0,0 +1,133 @@
+##' point_cut
+##'
+##' caculate cut point for mutiple genes
+##'
+##' @param exprSet_hub a tumor expression set for hubgenes
+##' @param meta meta data corresponds to expression set
+##' @importFrom survminer surv_cutpoint
+##' @export
+##' @return a vector with cutpoint for genes
+##' @author Xiaojie Sun
+##' @examples
+##' point_cut(exprSet_hub1,meta1)
+##' @seealso
+##' \code{\link{geo_download}};\code{\link{draw_volcano}};\code{\link{draw_venn}}
+
+point_cut <- function(exprSet_hub,meta){
+  if(ncol(exprSet_hub)!=nrow(meta))stop("your exprSet_hub is not corresponds to meta")
+  dat = cbind(t(exprSet_hub),meta)
+  cut.point = c()
+  for(i in 1:nrow(exprSet_hub)){
+    cut = survminer::surv_cutpoint(
+      dat,
+      time = "time",
+      event = "event",
+      variables = rownames(exprSet_hub)[i]
+    )
+    cut.point[[i]] = cut[["cutpoint"]][1,1]
+  }
+  names(cut.point) = rownames(exprSet_hub)
+  return(cut.point)
+}
+
+##' surv_KM
+##'
+##' caculate log_rank test p values for genes
+##'
+##' @inheritParams point_cut
+##' @param cut.point logical , use cut_point or not, if FALSE,use median by defult
+##' @param pvalue_cutoff p value cut off ,0.05 by defult
+##' @importFrom survival Surv
+##' @importFrom survival survdiff
+##' @export
+##' @return a vector with gene names and log_rank p value
+##' @author Xiaojie Sun
+##' @examples
+##' surv_KM(exprSet_hub1,meta1)
+##' surv_KM(exprSet_hub1,meta1,cut.point = T)
+##' @seealso
+##' \code{\link{geo_download}};\code{\link{draw_volcano}};\code{\link{draw_venn}}
+
+
+surv_KM <- function(exprSet_hub,meta,cut.point = F,pvalue_cutoff = 0.05){
+  cut_point = point_cut(exprSet_hub,meta)
+  log_rank_p = c()
+  for(i in 1:nrow(exprSet_hub)){
+    gene = as.numeric(exprSet_hub[i,])
+    if(cut.point){
+      meta$group=ifelse(gene>cut_point[i],'high','low')
+    }else{
+      meta$group=ifelse(gene>median(gene),'high','low')
+    }
+    data.survdiff=survival::survdiff(survival::Surv(time, event)~group,data=meta)
+    log_rank_p[[i]] = 1 - pchisq(data.survdiff$chisq, length(data.survdiff$n) - 1)
+  }
+  names(log_rank_p) = rownames(exprSet_hub)
+  log_rank_p=sort(log_rank_p)
+  tp = log_rank_p[log_rank_p<pvalue_cutoff]
+  return(tp)
+  message(paste0(length(tp)," of ",nrow(exprSet_hub)," genes selected"))
+}
+
+##' surv_cox
+##'
+##' caculate cox p values and HR for genes
+##'
+##' @inheritParams surv_cox
+##' @param HRkeep one of "all","protect"or"risk"
+##' @importFrom survival Surv
+##' @importFrom survival coxph
+##' @export
+##' @return a matrix with gene names ,cox p value and HR
+##' @author Xiaojie Sun
+##' @examples
+##' surv_cox(exprSet_hub1,meta1,cut.point = T,HRkeep = "all")
+##' @seealso
+##' \code{\link{geo_download}};\code{\link{draw_volcano}};\code{\link{draw_venn}}
+
+
+surv_cox <-function(exprSet_hub,meta,cut.point = F,pvalue_cutoff = 0.05,HRkeep = "all"){
+  cut_point = point_cut(exprSet_hub,meta)
+  cox_results <-list()
+  for(i in 1:nrow(exprSet_hub)){
+    #i = 1
+    gene= as.numeric(exprSet_hub[i,])
+    if(cut.point){
+      meta$group=ifelse(gene>cut_point[i],'high','low')
+    }else{
+      meta$group=ifelse(gene>median(gene),'high','low')
+    }
+    m=survival::coxph(survival::Surv(time, event) ~　group, data =  meta)
+
+    beta <- coef(m)
+    se <- sqrt(diag(vcov(m)))
+    HR <- exp(beta)
+    HRse <- HR * se
+
+    #summary(m)
+    tmp <- round(cbind(coef = beta, se = se, z = beta/se, p = 1 - pchisq((beta/se)^2, 1),
+                       HR = HR, HRse = HRse,
+                       HRz = (HR - 1) / HRse, HRp = 1 - pchisq(((HR - 1)/HRse)^2, 1),
+                       HRCILL = exp(beta - qnorm(.975, 0, 1) * se),
+                       HRCIUL = exp(beta + qnorm(.975, 0, 1) * se)), 3)
+    cox_results[[i]] = tmp['grouplow',]
+  }
+  cox_results = do.call(cbind,cox_results)
+  cox_results=t(cox_results)
+  rownames(cox_results)= rownames(exprSet_hub)
+  cox_results=cox_results[cox_results[,4]<pvalue_cutoff,]
+  if(HRkeep == "all"){
+    return(cox_results)
+    message(paste0(nrow(cox_results)," of ",nrow(exprSet_hub)," genes selected"))
+  }else if(HRkeep == "protect"){
+    k = cox_results[,5]<1
+    return(cox_results[k])
+    message(paste0(sum(k)," of ",nrow(exprSet_hub)," genes selected"))
+  }else if(HRkeep == "risk"){
+    k = cox_results[,5]>1
+    return(cox_results[k])
+    message(paste0(sum(k)," of ",nrow(exprSet_hub)," genes selected"))
+  }else if(!(HRkeep %in% c("all","protect","risk"))){
+    stop('HRkeep should be one of "all","protect"or"risk"')
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # tinyarray
-plots function for array
+
+### 前言
+
+hi，我是小洁。这是我基于自己的数据分析需求写的R包，很高兴被你看到了。我会在公众号《生信星球》更新这里面一些好用的小函数，也做一些其他的分享。
+
+### 函数介绍
+geo_download() : 提供geo编号，返回表达矩阵、临床信息表格和使用的平台编号。
+get_deg() ：提供芯片表达矩阵、分组信息、探针注释，返回差异分析结果。
+multi_deg() : 多个分组（最多5个）的差异分析
+cor.full()和cor.one() :批量计算基因间的相关性
+几个绘图函数：draw_heatmap,draw_volcano,draw_venn
+trans_exp():将tcga或tcga+gtex数据进行id转换
+t_choose():批量做单个基因的t检验
+point_cut():批量计算生存分析最佳截点
+surv_KM():批量做KM生存分析，支持用最佳截点分组
+surv_cox():批量做cox生存分析，支持用最佳截点分组
+
+### 尚未发表，还会继续更新
